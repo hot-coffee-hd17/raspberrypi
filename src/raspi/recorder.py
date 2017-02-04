8d77c918a3194dc2e8899da5686215374d4194ac
shun91 <shun9167@gmail.com>
2017-02-04 17:20:35 +0900

    fix

--- src/raspi/recorder.py
+++ src/raspi/recorder.py
@@ -1,10 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pyaudio
+import sys
+import time
+import wave
+from multiprocessing import Process
 
+RATE = 44100 # サンプリングレート、マイク性能に依存
+CHUNK = 1024
 
 class Recorder(object):
     def __init__(self):
-        pass
-    
+        # pyaudio
+        self.p = pyaudio.PyAudio()
+
+        # マイクからデータ取得
+        self.stream = self.p.open(
+            format = pyaudio.paInt16,
+            channels = 1,
+            rate = RATE,
+            input = True,
+            frames_per_buffer = CHUNK
+            )
+
+        # 録音データを保存する
+        self.all = []
+
+    def record(self):
+        for i in range(0, int(RATE / CHUNK * 3)):
+            data = self.stream.read(CHUNK)
+            self.all.append(data)
+
     def action(self):
         print('record!')
+        
+        self.record()
+         
+        self.stream.close()
+        data = b''.join(self.all)
+        out = wave.open('mono.wav','w')
+        out.setnchannels(1) #mono
+        out.setsampwidth(2) #16bits
+        out.setframerate(RATE)
+        out.writeframes(data)
+        out.close()
+         
+        self.p.terminate()
