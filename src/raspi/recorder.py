4768ccc2bb29cf6c76c46764d72d06e0b0b2a369
shun91 <shun9167@gmail.com>
2017-02-05 01:44:06 +0900

    マイクのインデックス変更

--- src/raspi/recorder.py
+++ src/raspi/recorder.py
@@ -48,6 +48,7 @@ class Recorder(object):
             channels = self.wf.getnchannels(),
             rate = self.wf.getframerate(),
             input_device_index = input_device_index,
+            frames_per_buffer=1024,
             input = True,
             stream_callback = self.callback
             )
