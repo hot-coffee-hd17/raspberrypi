bdf66a67142f5e82a5581e99750b5188ff74d25f
UekusaTomoki <tmkueks19@gmail.com>
2017-02-04 16:08:12 +0900

    音楽再生機能の追加

---
+++ src/raspi/bone_sound.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import wave
+import pyaudio
+import time
+
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+class BoneSound :
+  def __init__(self):
+    # チャンク数を指定
+    self.CHUNK = 1024
+    self.FILENAME = "/Users/tmk-mac/Development/raspberrypi/resource/decision1.wav"
+    self.wf = wave.open(self.FILENAME, "rb")
+
+    # PyAudioのインスタンスを生成
+    self.pyaudio_instance = pyaudio.PyAudio()
+    # Streamを生成
+    """
+     format: ストリームを読み書きする際のデータ型
+     channels: モノラルだと1、ステレオだと2、それ以外の数字は入らない
+     rate: サンプル周波数
+     output: 出力モード
+    """
+    self.stream = self.pyaudio_instance.open(
+      format=self.pyaudio_instance.get_format_from_width(self.wf.getsampwidth()),
+      channels=self.wf.getnchannels(),
+      rate=self.wf.getframerate(),
+      output=True)
+
+  # play sound
+  def action(self):
+    print("Sound Playing")
+    # データを1度に1024個読み取る
+    data = self.wf.readframes(self.CHUNK)
+
+    # 実行
+    while data != b'':
+      self.stream.write(data)
+      data = self.wf.readframes(self.CHUNK)
+
+    self.stream.stop_stream()
+    self.stream.close()
+
+    self.pyaudio_instance.terminate()
