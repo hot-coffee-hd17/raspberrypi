04133e6c6b49f0d117c48f4ea792dd872cce4473
shun91 <shun9167@gmail.com>
2017-02-04 15:08:36 +0900

    mainを少し修正

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
+import re
 from raspi import sensor
 from raspi import gesture
 
 # 何秒毎にセンサーをチェックするか
 FRAME_RATE = 1
+
+
+def to_snakecase(str):
+    ret = re.sub(r'([\s|A-Z])', "_\\1", str)
+    ret = re.sub(r'([\s])', "",ret)
+    ret = re.sub(r'^_', "",ret)
+    return ret.lower()
 
 
 def main():
@@ -18,7 +26,7 @@ def main():
         
         # ジェスチャーであると判定されたら対応するアクション実行
         if class_name is not None:
-            mod = __import__('raspi.' + class_name.lower(), fromlist=[class_name])
+            mod = __import__('raspi.' + to_snakecase(class_name), fromlist=[class_name])
             instance = getattr(mod, class_name)()
             instance.action()
 
