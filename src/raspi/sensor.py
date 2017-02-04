11be5dd98e534e46f2e4fae055faa229e0e088d7
shun91 <shun9167@gmail.com>
2017-02-04 22:17:57 +0900

    modify sensor

--- src/raspi/sensor.py
+++ src/raspi/sensor.py
@@ -1,14 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import RPi.GPIO as GPIO
+import numpy as np
+
+# 圧力センサーの最大値・最小値（正規化に利用）
+PRESS_MAX = 100
+PRESS_MIN = 0
+
+def rand_val():
+    ''' テスト用 '''
+    import random
+    return np.array([random.randint(PRESS_MIN, PRESS_MAX), random.randint(PRESS_MIN, PRESS_MAX)])
+
+
+def from_sensors():
+    import spidev
+
+    # 人差し指
+    spi1 = spidev.SpiDev()
+    spi1.open(0, 0)
+    res1 = spi1.xfer2([0x68, 0x00])
+    val1 = (res1[0] * 256 + res1[1]) & 0x3ff
+
+    # # 小指
+    # spi2 = spidev.SpiDev()
+    # spi2.open(0, 0)
+    # res2 = spi2.xfer2([0x68, 0x00])
+    # val2 = (res1[0] * 256 + res2[1]) & 0x3ff
+    #
+    # return np.array([val1, val2])
+
+    return np.array([val1, val1])
+
 
 def get_values():
     '''
     全てのセンサーから値を取得して返す
     人差し指・小指の順
     '''
-    from random import random
-    return [random(), random()]
+    pressure = rand_val()
+
+    # 正規化して返す
+    return list((pressure - PRESS_MIN) / (PRESS_MAX - PRESS_MIN))
 
 
 def main():
