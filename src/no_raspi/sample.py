c6da41131e9e92db6fb82363e1a475fc448a362b
shun91 <shun9167@gmail.com>
2017-02-04 13:11:12 +0900

    fix

--- src/no_raspi/sample.py
+++ src/no_raspi/sample.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
 
+COUNT = 3
+PIN = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN,GPIO.OUT)
 
-def main():
-    print('no_raspi')
+for _ in range(COUNT):
+    GPIO.output(PIN,True)
+    time.sleep(1.0)
+    GPIO.output(PIN,False)
+    time.sleep(1.0)
 
-
-if __name__ == '__main__':
-    main()
+GPIO.cleanup()
